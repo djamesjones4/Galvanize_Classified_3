e3e0624b6971d2f85377d107bf93ebd06b325687
Derek Jones <djamesjones4@gmail.com>
2017-06-06 16:23:36 -0600

    committing again

--- migrations/20170517113105_classifieds.js
+++ migrations/20170517113105_classifieds.js
@@ -5,7 +5,7 @@ exports.up = function(knex, Promise) {
     table.string('description').notNullable()
     table.decimal('price').notNullable()
     table.string('item_image').notNullable()
-    table.timestamps(true, true).defaultTo(knex.fn.now())
+    table.timestamps(true, true)
   })
 }
 
